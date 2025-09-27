4d7bbc8833dcf3f645f17d00a397d9f84e3ba4cd
Yash Raj <yashproraj@gmail.com>
2025-09-27 23:58:26 +0530

    add college.controller.js with addClass function

---
+++ src/controllers/college.controller.js
@@ -0,0 +1,21 @@
+import Class from "../models/class.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const addClass = asyncHandler(async (req, res) => {
+  const { className } = req.body;
+  if (!className || typeof className !== "string" || className.trim() === "") {
+    throw new ApiError(400, "Class name must be a non-empty string");
+  }
+
+  className = className.trim();
+  const classExists = await Class.findOne({ className });
+  if (classExists)
+    throw new ApiError(400, "Class with this name already exists");
+
+  const newClass = await Class.create({ className });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, newClass, "Class created successfully"));
+});
