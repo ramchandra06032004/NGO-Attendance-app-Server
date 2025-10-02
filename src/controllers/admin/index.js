ec709185fdc92e3d81fa7599cee6c41f91da313f
Yash Raj <yashproraj@gmail.com>
2025-10-02 18:03:57 +0530

    add: Centralized export for auth and admin

---
+++ src/controllers/admin/index.js
@@ -0,0 +1,5 @@
+import addCollege from "./addCollege.js";
+import addNgo from "./addNgo.js";
+import registerAdmin from "./registerAdmin.js";
+
+export { addCollege, addNgo, registerAdmin };
---
+++ src/controllers/auth/index.js
@@ -0,0 +1,5 @@
+import login from "./login.js";
+import logout from "./logout.js";
+import refreshAccessToken from "./refreshAccessToken.js";
+
+export { login, logout, refreshAccessToken };
